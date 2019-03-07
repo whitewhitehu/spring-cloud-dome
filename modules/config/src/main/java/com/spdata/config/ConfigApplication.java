a23ed5fe084c6beb867b3cf6b334f197f7bcd72b
yangqifang <1114923161@qq.com>
2019-03-07 20:07:46 +0800

    添加config配置服务中心

---
+++ modules/config/src/main/java/com/spdata/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.spdata.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author yangqifang
+ * @date 2019年3月7日
+ * @deprecated 配置服务 启动
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
